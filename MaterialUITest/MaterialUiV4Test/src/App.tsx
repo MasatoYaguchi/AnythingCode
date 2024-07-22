823706c6c79a6e5bf7d585b69071e3fdda057632
MasatoYaguchi <ygcmst0510@gmail.com>
2024-07-22 14:54:19 +0900

    update

--- MaterialUITest/MaterialUiV4Test/src/App.tsx
+++ MaterialUITest/MaterialUiV4Test/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-
+// import { useSearchParams } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>テストページ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- MaterialUITest/MaterialUiV4Test/vite.config.ts
+++ MaterialUITest/MaterialUiV4Test/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRefresh(), mkcert()],
+  server: {
+    host: true,
+    // https: true,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+});
